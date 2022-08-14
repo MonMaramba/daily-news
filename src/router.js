b147a4e70ce1662a08a24c64fed1bc2614c2469f
MonMaramba <monmaramba@yahoo.com>
2022-08-13 21:38:36 -0700

    added css, created header and figuring out react-router-bootstrap for links

--- src/router.js
+++ src/router.js
@@ -3,12 +3,13 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './components/home';
 import Contact from './components/contact';
 import PostComponent from './components/posts';
+import Header from './components/header';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        Yikes!!!!
         <Route path='/' element={<Home />} />
         <Route path='contact' element={<Contact />} />
         <Route path='article/:id' element={<PostComponent />} />
