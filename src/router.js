154d001816913f4e6ab79e0bb82fcc5abe52df6f
MonMaramba <monmaramba@yahoo.com>
2022-08-14 14:06:46 -0700

    Created MainLayout to wrap everything. added google fonts and toastify

--- src/router.js
+++ src/router.js
@@ -4,16 +4,19 @@ import Home from './components/home';
 import Contact from './components/contact';
 import PostComponent from './components/posts';
 import Header from './components/header';
+import MainLayout from './layouts/mainLayout';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='contact' element={<Contact />} />
-        <Route path='article/:id' element={<PostComponent />} />
-      </Routes>
+      <MainLayout>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='article/:id' element={<PostComponent />} />
+        </Routes>
+      </MainLayout>
     </BrowserRouter>
   );
 };
