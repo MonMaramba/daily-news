e3ba44c0bb09076a1aaae5879616d644deab19ba
MonMaramba <monmaramba@yahoo.com>
2022-08-21 18:58:53 -0700

    added formik for forms. Started contacts page forms

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,5 +1,87 @@
+import { useDispatch } from 'react-redux';
+
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Alert } from 'react-bootstrap';
+
 const Contact = () => {
-  return <>CONTACT</>;
+  const dispatch = useDispatch();
+  const formik = useFormik({
+    initialValues: { email: '', firstname: '', lastname: '', message: '' },
+    validationSchema: Yup.object({
+      email: Yup.string().required('sorry the is required'),
+      firstname: Yup.string().required('sorry the name is required'),
+      lastname: Yup.string().required('Sorry the last name is required'),
+      message: Yup.string()
+        .required('Sorry the message is required')
+        .max(500, 'Sorry the message is too long'),
+    }),
+    onSubmit: (values, { resetForm }) => {
+      console.log(values);
+    },
+  });
+
+  return (
+    <>
+      <h1>Contact Us</h1>
+      <form className='mt-3' onSubmit={formik.handleSubmit}>
+        <div className='form-group'>
+          <label htmlFor='email'>Email address</label>
+          <input
+            type='email'
+            className='form-control'
+            name='email'
+            placeholder='email@example.com'
+            {...formik.getFieldProps('email')}
+          />
+          {formik.errors.email && formik.touched.email ? (
+            <Alert variant='danger'>{formik.errors.email}</Alert>
+          ) : null}
+        </div>
+        <div className='form-group'>
+          <label htmlFor='firstname'>First name</label>
+          <input
+            type='text'
+            className='form-control'
+            name='firstname'
+            placeholder='Enter the firstname'
+            {...formik.getFieldProps('firstname')}
+          />
+          {formik.errors.firstname && formik.touched.firstname ? (
+            <Alert variant='danger'>{formik.errors.firstname}</Alert>
+          ) : null}
+        </div>
+        <div className='form-group'>
+          <label htmlFor='lastname'>Last name</label>
+          <input
+            type='text'
+            className='form-control'
+            name='lastname'
+            placeholder='Enter the lastname'
+            {...formik.getFieldProps('lastname')}
+          />
+          {formik.errors.lastname && formik.touched.lastname ? (
+            <Alert variant='danger'>{formik.errors.lastname}</Alert>
+          ) : null}
+        </div>
+        <div className='form-group mt-2'>
+          <label htmlFor='message'>Message</label>
+          <textarea
+            className='form-control'
+            name='message'
+            rows={3}
+            {...formik.getFieldProps('message')}
+          />
+          {formik.errors.message && formik.touched.message ? (
+            <Alert variant='danger'>{formik.errors.message}</Alert>
+          ) : null}
+        </div>
+        <button type='submit' className='btn btn-primary mt-2'>
+          Send message
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default Contact;
